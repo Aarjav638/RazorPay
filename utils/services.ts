8432b5acf12d98d3741869e02b2f38d3aba6e2c0
Aarjav Jain <anshjain638@gmail.com>
2024-10-11 15:58:20 +0530

    feat: added implementation to initiate payment and create orderId

---
+++ utils/services.ts
@@ -0,0 +1,81 @@
+import {Alert} from 'react-native';
+import RazorpayCheckout from 'react-native-razorpay';
+type userDetails = {
+  fullname: string;
+  address: string;
+  city: string;
+  pincode: string;
+  email: string;
+  contact: string;
+  totalprice: number;
+};
+
+const orderIds = new Set();
+
+async function generateUniqueOrderId(length = 10) {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let orderId;
+
+  do {
+    orderId = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      orderId += characters[randomIndex];
+    }
+  } while (orderIds.has(orderId));
+
+  orderIds.add(orderId);
+  return orderId;
+}
+
+export async function makePayment({
+  fullname,
+  address,
+  city,
+  pincode,
+  email,
+  contact,
+
+  totalprice,
+}: userDetails) {
+  let orderid = await generateUniqueOrderId();
+
+  var options = {
+    description: 'Sample Payment',
+    image: require('../assets/razorpay-icon.svg'),
+    order_id: orderid,
+    currency: 'INR',
+    key: '<api-key>',
+    amount: totalprice,
+    name: 'Razorpay',
+    prefill: {
+      email,
+      name: fullname,
+      contact,
+    },
+    theme: {color: '#B03F82'},
+  };
+
+  if (
+    !fullname ||
+    !address.split(' ')[0] ||
+    !city ||
+    !pincode ||
+    !email ||
+    !contact
+  ) {
+    throw new Error('Enter the details first!');
+  } else {
+    try {
+      const res = await RazorpayCheckout.open(options);
+      Alert.alert(`Success: ${res.razorpay_payment_id}`);
+      return {paymentId: res.razorpay_payment_id, orderId: orderid};
+    } catch (err) {
+      const errorMessage =
+        (err as {error: {description: string}})?.error.description ||
+        'Payment failed';
+      throw new Error(errorMessage);
+    }
+  }
+}
